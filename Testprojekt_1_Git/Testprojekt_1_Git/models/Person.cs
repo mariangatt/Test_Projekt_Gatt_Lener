1abf137f0793bab1c4518e23262b36ccdc03bb78
JannikLener <45951676+JannikLener@users.noreply.github.com>
2019-01-24 10:36:19 +0100

    Überarbeitetes Projekt Lener

--- Testprojekt_1_Git/Testprojekt_1_Git/models/Person.cs
+++ Testprojekt_1_Git/Testprojekt_1_Git/models/Person.cs
@@ -11,6 +11,7 @@ namespace Testprojekt_1_Git.models
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public int Age { get; set; }
+        public decimal Salary { get; set; }
 
     }
 }
