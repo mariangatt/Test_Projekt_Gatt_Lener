838f7ee1e16756f22139fe57edaf193a8df28794
JannikLener <45951676+JannikLener@users.noreply.github.com>
2019-01-24 10:29:44 +0100

    VisualStudio2017 Projekt

---
+++ Testprojekt_1_Git/Testprojekt_1_Git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testprojekt_1_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo, dies ist unser erstes Projekt");
+        }
+    }
+}
---
+++ Testprojekt_1_Git/Testprojekt_1_Git/models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testprojekt_1_Git.models
+{
+    class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+    }
+}
